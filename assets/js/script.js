1ee39e390883dca9cfebe847a94715bb38337b1a
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-02-29 13:48:14 -0800

    updated generate password function to include prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,8 @@
-// Assignment code here
-
-// User clicks generate password button, user is presented with the following prompts: 
-//     User clicks generate password button
-//     Prompt asking for length of password
-//     User inputs a length at least 8 chac. and no more than 128
-//     Prompt asking for character types to include: lowercase, uppercase, numeric, and/or special characters
-//     User inputs character types
-//     Prompt confirms selections
-//     User confirms selections?
-//     Password generated
-//     Password in an alert or displayed in the space on the page to copy
-
-// Get references to the #generate element
-
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
-  
-//if invalid var userInput = window.prompt("How long would you like your password to be? (must be at least 8 characters long)");
-    //if(userInput === 0, 1)  
-  
- //window.confirm(message)
-  
-// Add event listener to generate button
-
-    // If user decides to stop by hitting cancel, this will end the function
-  //if (!userInput) {
-      //return;
 
 // Write password to the #password input
-
-function writePassword() {
-
-  var writePassword = Math.random()
-
-
-}
-  
-generateBtn.addEventListener("click", ); {
+function generatePassword() {
 
   var passwordLength = window.prompt("How long would you like your password to be? (must be at least 8 characters long)");
   console.log(passwordLength);
@@ -52,7 +19,31 @@ generateBtn.addEventListener("click", ); {
   var passwordSpecialch = window.prompt("Would you like special characters in your password?");
   console.log(passwordSpecialch)
 
-}
+  return("")
+};
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+};
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
-//addEventListener('click', function() { // Display the prompt when the button is clicked const userInput = prompt('Please enter some text:'); console. log(userInput); });
+// User clicks generate password button, user is presented with the following prompts: 
+//     User clicks generate password button
+//     Prompt asking for length of password
+//     User inputs a length at least 8 chac. and no more than 128
+//     Prompt asking for character types to include: lowercase, uppercase, numeric, and/or special characters
+//     User inputs character types
+//     Prompt confirms selections
+//     User confirms selections?
+//     Password generated
+//     Password in an alert or displayed in the space on the page to copy
+
+// Get references to the #generate element
+
