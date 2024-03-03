97c9d7b901449617adcd45f789e4cfd3aea35582
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-03-03 12:29:05 -0800

    updated code to make the generate password button work + add character sets for password generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,71 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordUpper;
+var passwordLower;
+var passwordNumber;
+var passwordSpecialch;
 
 // Write password to the #password input
 function generatePassword() {
 
-  var passwordLength = window.prompt("How long would you like your password to be? (must be at least 8 characters long)");
-  console.log(passwordLength);
+    var passwordLength = window.prompt("How long would you like your password to be? (must be at least 8 characters long)");
+        console.log(passwordLength);
+            if (Number.isNaN(parseInt(passwordLength)) || passwordLength < 8 || passwordLength > 128) {
+                alert("Please enter a number between 8 - 128.");
+                return "Please click generate password again."
+            } 
 
-  var passwordUpper = window.prompt("Would you like uppercase letters in your password?");
-  console.log(passwordUpper);
+    passwordUpper = window.confirm("Would you like uppercase letters in your password?  (click OK for yes, and CANCEL for no)");
+        console.log(passwordUpper);
 
-  var passwordLower = window.prompt("Would you like lowercase letters in your password?");
-  console.log(passwordLower);
+    passwordLower = window.confirm("Would you like lowercase letters in your password? (click OK for yes, and CANCEL for no)");
+        console.log(passwordLower);
 
-  var passwordNumber = window.prompt("Would you like numbers in your password?");
-  console.log(passwordNumber)
+    passwordNumber = window.confirm("Would you like numbers in your password? (click OK for yes, and CANCEL for no)");
+        console.log(passwordNumber)
 
-  var passwordSpecialch = window.prompt("Would you like special characters in your password?");
-  console.log(passwordSpecialch)
+    passwordSpecialch = window.confirm("Would you like special characters in your password? (click OK for yes, and CANCEL for no)");
+        console.log(passwordSpecialch)
 
-  return("")
+  passwordChoices();
 };
+
+function passwordChoices() {
+
+        var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        var passNumeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        var specialChar = ['!', '#', '$', '%', '&', '\'', '()', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', '^', '_', '`', '{', '|', '}', '~', '"'];
+        var combineArr = [] 
+
+            if (passwordUpper === true) {
+                    combineArr = combineArr.concat(upperCase)
+            }
+
+            if (passwordLower === true) {
+                combineArr = combineArr.concat(lowerCase)
+            }
+            
+            if (passwordNumber === true) {
+                combineArr = combineArr.concat(passNumeric)
+            }
+
+            if (passwordSpecialch === true) {
+                combineArr = combineArr.concat(specialChar)
+                console.log(typeof specialChar);
+            }
+
+            console.log(combineArr);
+            console.log(typeof upperCase);
+            console.log(typeof lowerCase);
+            console.log(typeof passNumeric);
+            console.log(typeof specialChar);
+           
+}
+
+
+
+
 
 function writePassword() {
   var password = generatePassword();
